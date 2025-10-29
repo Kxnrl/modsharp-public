c1a2aa4c670ed72bde0fc0071bf2c39169c80f3f
Nuko <me@nuko.me>
2025-10-30 01:05:20 +0800

    123

--- Engine/src/cstrike/interface/ISchemaSystem.h
+++ Engine/src/cstrike/interface/ISchemaSystem.h
@@ -107,6 +107,26 @@ struct SchemaClassInfoData_t
         return m_BaseClasses->m_pClass;
     }
 
+    int8_t GetBaseClassSize() const
+    {
+        return m_nNumBaseClasses;
+    }
+
+    SchemaBaseClassInfoData_t* GetBaseClasses() const
+    {
+        return m_BaseClasses;
+    }
+
+    int16_t GetMetadataSize() const
+    {
+        return m_nStaticMetadataCount;
+    }
+
+    SchemaMetadataEntryData_t* GetStaticMetadata() const
+    {
+        return m_pStaticMetadata;
+    }
+
     SchemaClassInfoData_t() = delete;
 
 private:
@@ -114,13 +134,19 @@ private:
     const char*                m_pszName;
     const char*                m_pszModule;
     int32_t                    m_nSize;
+
     int16_t                    m_nNumFields;
+    int16_t                    m_nStaticMetadataCount;
+
     int8_t                     m_nAlignOf;
     int8_t                     m_nNumBaseClasses;
+
     int16_t                    m_nMultipleInheritanceDepth;
     int16_t                    m_nSingleInheritanceDepth;
     SchemaClassFieldData_t*    m_pFields;
     SchemaBaseClassInfoData_t* m_BaseClasses;
+    void*                      m_pDataMap;
+    SchemaMetadataEntryData_t* m_pStaticMetadata;
 };
 
 class CSchemaSystemTypeScope
