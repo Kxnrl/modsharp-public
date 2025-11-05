ce839ec96ae8737cc1b44738e8d9857472bc0403
BoneTM <bonetm@qq.com>
2025-11-06 00:47:43 +0800

    init

---
+++ Sharp.Modules/InputManager/Shared/IInputManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Sharp.Shared.Objects;
+
+namespace Sharp.Modules.InputManager.Shared;
+
+public interface IInputManager
+{
+    const string Identity = nameof(IInputManager);
+
+    public IDisposable AddInputListener(InputKey key, Action<IGameClient> callback);
+}
