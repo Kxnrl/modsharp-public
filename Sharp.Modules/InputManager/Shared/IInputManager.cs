4bcccfedfd119b1f5645e9035ff1821d492f13a3
BoneTM <bonetm@qq.com>
2025-11-06 13:36:24 +0800

    Add next chat listener support to InputManager

--- Sharp.Modules/InputManager/Shared/IInputManager.cs
+++ Sharp.Modules/InputManager/Shared/IInputManager.cs
@@ -8,4 +8,8 @@ public interface IInputManager
     const string Identity = nameof(IInputManager);
 
     public IDisposable AddInputListener(InputKey key, Action<IGameClient> callback);
+
+    public IDisposable GetClientNextChat(IGameClient client, Action<IGameClient, string> callback, bool handleChat = true);
+
+    public void CancelClientNextChat(IGameClient client);
 }
