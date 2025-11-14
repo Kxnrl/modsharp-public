5eae5ba298507bcaf96f8dfa2e2774c51171d84e
Kyle <kyle@kxnrl.com>
2025-11-14 21:27:21 +0800

    [skip ci] native call

--- docfx/docs/codes/native-call.cs
+++ docfx/docs/codes/native-call.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Sharp.Shared;
@@ -5,53 +7,104 @@ using Sharp.Shared.Attributes;
 using Sharp.Shared.Calls;
 using Sharp.Shared.GameEntities;
 using Sharp.Shared.GameObjects;
+using Sharp.Shared.Types;
+
+[assembly: DisableRuntimeMarshalling]
 
 namespace NativeCallExample;
 
 internal unsafe interface IBaseEntityVirtualCall : IVirtualCall<IBaseEntity>
 {
     // Same as signature call, but this one uses vtable index instead of signature.
-    void* GetDynamicBinding(nint instance, void** result);
+    bool PassesDamageFilter(IBaseEntity instance, TakeDamageInfo* pInfo);
 }
 
 internal interface IWeaponServiceSignatureCall : ISignatureCall<IWeaponService>
 {
     // in gamedata it will be CCSPlayer_WeaponServices::GetWeaponByName
-    // WHere does CCSPlayer_WeaponServices comes from? you can F12 (if you use Visual Studio) then see NetClass attribute.
+    // Where does CCSPlayer_WeaponServices comes from? you can F12 (if you use Visual Studio) then see NetClass attribute.
     // this is the class name what we need.
+    // if your function name in gamedata mismatch with method name, you can use AddressKey to specify it.
+    // by default, it always uses {NetClass}::{MethodName} to find the signature.
     [AddressKey("GetWeaponByName")]
     nint GetWeaponByName(IWeaponService weaponService, string weaponName);
 }
 
+// you also can define without type parameter
+internal interface IWeaponServiceSignatureCallWithoutType : ISignatureCall
+{
+    [AddressKey("CCSPlayer_WeaponServices::GetWeaponByName")]
+    nint GetWeaponByName(IWeaponService weaponService, string weaponName);
+
+    [AddressKey("CCSPlayer_WeaponServices::GetWeaponByName")]
+    nint GetWeaponByNameUsingPointer(nint weaponService, string weaponName);
+}
+
 internal class NativeCallExample : IModSharpModule
 {
+    private readonly ISharedSystem _sharedSystem;
+
     // How to invoke them is ignored.
-    private readonly IBaseEntityVirtualCall _vCall;
+    private readonly IBaseEntityVirtualCall      _vCall;
     private readonly IWeaponServiceSignatureCall _sigCall;
-    public NativeCallExample(ISharedSystem sharedSystem, string dllPath, string sharpPath, Version version, IConfiguration coreConfiguration, bool hotReload)
+
+    public NativeCallExample(ISharedSystem sharedSystem,
+        string                             dllPath,
+        string                             sharpPath,
+        Version                            version,
+        IConfiguration                     coreConfiguration,
+        bool                               hotReload)
     {
+        _sharedSystem = sharedSystem;
+
+        sharedSystem.GetModSharp().GetGameData().Register("native-call.games");
+
         // For concept validation, you can directly construct and call.
-        _vCall = new BaseEntityVirtualCall(sharedSystem.GetModSharp().GetGameData());
+        _vCall   = new BaseEntityVirtualCall(sharedSystem.GetModSharp().GetGameData());
         _sigCall = new WeaponServiceSignatureCall(sharedSystem.GetModSharp().GetGameData());
 
         // For a real project, you should always use dependency injection!
         var services = new ServiceCollection();
-        // You must add this, otherwise native call will fail to construct.
+
+        // You must add GameData, otherwise native call will fail to construct.
         services.AddSingleton(sharedSystem.GetModSharp().GetGameData());
-        services.AddSingleton<BaseEntityVirtualCall>();
-        services.AddSingleton<WeaponServiceSignatureCall>();
+
+        services.AddSingleton<IBaseEntityVirtualCall, BaseEntityVirtualCall>();
+        services.AddSingleton<IWeaponServiceSignatureCall, WeaponServiceSignatureCall>();
 
         var provider = services.BuildServiceProvider();
-        provider.GetRequiredService<BaseEntityVirtualCall>();
-        provider.GetRequiredService<WeaponServiceSignatureCall>();
+        _vCall   = provider.GetRequiredService<BaseEntityVirtualCall>();
+        _sigCall = provider.GetRequiredService<WeaponServiceSignatureCall>();
     }
 
-    public bool Init() => true;
+    public bool Init()
+        => true;
 
     public void Shutdown()
     {
+        _sharedSystem.GetModSharp().GetGameData().Register("native-call.games");
     }
 
-    public string DisplayName => "Native Call Example";
+    private IBaseWeapon? GetWeaponByName(IPlayerPawn pawn)
+    {
+        if (pawn.GetWeaponService() is not { } ws)
+        {
+            return null;
+        }
+
+        var pWeapon = _sigCall.GetWeaponByName(ws, "weapon_ak47");
+
+        return _sharedSystem.GetModSharp().CreateNativeObject<IBaseWeapon>(pWeapon);
+    }
+
+    private unsafe bool PassesDamageFilter(IBaseEntity entity, in TakeDamageInfo info)
+    {
+        fixed (TakeDamageInfo* pInfo = &info)
+        {
+            return _vCall.PassesDamageFilter(entity, pInfo);
+        }
+    }
+
+    public string DisplayName   => "Native Call Example";
     public string DisplayAuthor => "ModSharp dev team";
 }
