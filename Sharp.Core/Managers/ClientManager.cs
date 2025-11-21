b55849d149459c9edcce18df53b197f27b45f12a
Kyle <kyle@kxnrl.com>
2025-11-22 02:09:50 +0800

    ✈️

--- Sharp.Core/Managers/ClientManager.cs
+++ Sharp.Core/Managers/ClientManager.cs
@@ -348,26 +348,6 @@ internal class ClientManager : ICoreClientManager
             return;
         }
 
-        if (_sReloadAdmin)
-        {
-            for (var i = 0; i < _listeners.Count; i++)
-            {
-                try
-                {
-                    _listeners[i].OnAdminCacheReload();
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e,
-                                     "An error occurred while calling listener<{s}> {name}",
-                                     "OnAdminCacheReload",
-                                     _listeners[i].GetType().Name);
-                }
-            }
-
-            _sReloadAdmin = false;
-        }
-
         if (_adminRunCheckQueue.Count == 0)
         {
             return;
@@ -384,15 +364,13 @@ internal class ClientManager : ICoreClientManager
 
             if (client.IsAuthenticated)
             {
-                var c = client;
-
                 var block = false;
 
                 for (var i = 0; i < _listeners.Count; i++)
                 {
                     try
                     {
-                        if (_listeners[i].OnClientPreAdminCheck(c))
+                        if (_listeners[i].OnClientPreAdminCheck(client))
                         {
                             block = true;
                         }
@@ -408,29 +386,31 @@ internal class ClientManager : ICoreClientManager
 
                 if (block)
                 {
-                    _logger.LogWarning("Blocked {client}<{steamId}> as AdminCheck", c.Name, c.SteamId);
+                    _logger.LogDebug("Blocked {client}<{steamId}> as AdminCheck", client.Name, client.SteamId);
 
                     continue;
                 }
-
-                for (var i = 0; i < _listeners.Count; i++)
-                {
-                    try
-                    {
-                        _listeners[i].OnClientPostAdminCheck(c);
-                    }
-                    catch (Exception e)
-                    {
-                        _logger.LogError(e,
-                                         "An error occurred while calling listener<{s}> {name}",
-                                         "OnClientPostAdminCheck",
-                                         _listeners[i].GetType().Name);
-                    }
-                }
             }
-            else
+            else if (!client.IsFakeClient)
             {
                 back.Add(client);
+
+                continue;
+            }
+
+            for (var i = 0; i < _listeners.Count; i++)
+            {
+                try
+                {
+                    _listeners[i].OnClientPostAdminCheck(client);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e,
+                                     "An error occurred while calling listener<{s}> {name}",
+                                     "OnClientPostAdminCheck",
+                                     _listeners[i].GetType().Name);
+                }
             }
         }
 
