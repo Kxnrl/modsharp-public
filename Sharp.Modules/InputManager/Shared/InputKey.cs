960748c28948ff3e2d9cc19cf6ae1b312bacd39f
laper32 <laper32@outlook.com>
2025-12-11 19:33:26 +0800

    docs: InputKey special info

--- Sharp.Modules/InputManager/Shared/InputKey.cs
+++ Sharp.Modules/InputManager/Shared/InputKey.cs
@@ -1,5 +1,11 @@
-﻿namespace Sharp.Modules.InputManager.Shared;
+using System;
 
+namespace Sharp.Modules.InputManager.Shared;
+
+/// <summary>
+/// 输入键位。 <br/>
+/// 这里的键位为「CS2本身的默认键位」，不考虑你改键的情况。换句话说这里都是「约定」。
+/// </summary>
 public enum InputKey
 {
     W,
@@ -12,6 +18,8 @@ public enum InputKey
     E,
     Space,
     Shift,
+    [Obsolete("Currently does nothing, DO NOT USE IT FOR NOW. It will be implemented in the future release. This is just a placeholder.")]
     F3,
+    [Obsolete("Currently does nothing, DO NOT USE IT FOR NOW. It will be implemented in the future release. This is just a placeholder.")]
     F4,
 }
