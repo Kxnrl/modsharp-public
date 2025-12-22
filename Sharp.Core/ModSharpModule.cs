b5be4517743d4174b59adc1ade2c154e87b5b564
Nukoooo <89713806+Nukoooo@users.noreply.github.com>
2025-12-22 16:07:14 +0800

    a

--- Sharp.Core/ModSharpModule.cs
+++ Sharp.Core/ModSharpModule.cs
@@ -23,6 +23,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using McMaster.NETCore.Plugins;
 using Microsoft.Extensions.Configuration;
 using Sharp.Core.Utilities;
