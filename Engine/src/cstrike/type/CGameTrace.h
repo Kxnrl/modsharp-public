33a5e961b538a3a785471406a5b0d9a6f347ee5b
Nukoooo <89713806+Nukoooo@users.noreply.github.com>
2026-01-22 21:50:34 +0800

    update pt.3

--- Engine/src/cstrike/type/CGameTrace.h
+++ Engine/src/cstrike/type/CGameTrace.h
@@ -469,6 +469,8 @@ public:
         m_bIgnoreIfBothInteractWithHitBoxes = false;
         m_bForceHitEverything               = false;
         m_bUnknown                          = true;
+
+        m_nUnk1 = 0xFFFF;
     }
 
     bool HasInteractsAsLayer(int nLayerIndex) const { return (m_nInteractsAs & (1ull << nLayerIndex)) != 0; }
@@ -496,6 +498,8 @@ public:
     CBaseHandle        m_nEntityIdsToIgnore[2]; // this is the ID of the game entity which should be ignored
     CBaseHandle        m_nOwnerIdsToIgnore[2];  // this is the ID of the owner of the game entity which should be ignored
     uint16_t           m_nHierarchyIds[2];      // this is an ID for the hierarchy of game entities (used to disable collision among objects in a hierarchy)
+    uint16_t           m_nUnk1 = 0xFFFF;
+    uint8_t            m_nUnk2;
     RnQueryObjectFlags m_nObjectSetMask;        // set of RnQueryObjectSet bits
     uint8_t            m_nCollisionGroup;       // one of the registered collision groups
 
