ab9de4c5daa19b4360812b09c94d4a1cb4dc17b4
Nuko <me@nuko.me>
2026-05-12 04:35:08 +0800

    make it editable

--- Sharp.Core/Objects/GameClient.cs
+++ Sharp.Core/Objects/GameClient.cs
@@ -205,7 +205,7 @@ internal partial class GameClient : NativeObject, IGameClient
 
         if (_showSurvivalRespawnStatusEvent is null)
         {
-            _showSurvivalRespawnStatusEvent = GameEvent.Create(Event.CreateEvent("show_survival_respawn_status", true))
+            _showSurvivalRespawnStatusEvent = GameEvent.CreateEditable(Event.CreateEvent("show_survival_respawn_status", true))
                                               ?? throw new Exception("Failed to create event show_survival_respawn_status");
 
             _showSurvivalRespawnStatusEvent.SetInt("duration", 1);
