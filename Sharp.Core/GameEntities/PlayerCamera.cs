6064c875c67cc41f0631d8d04be242d10ef1970a
2vg <mail@nyaa.gg>
2026-09-11 13:50:09 +0900

    feat: sync

---
+++ Sharp.Core/GameEntities/PlayerCamera.cs
@@ -0,0 +1,63 @@
+/*
+ * ModSharp
+ * Copyright (C) 2023-2026 Kxnrl. All Rights Reserved.
+ *
+ * This file is part of ModSharp.
+ * ModSharp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ModSharp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ModSharp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using Sharp.Core.Utilities;
+using Sharp.Generator;
+using Sharp.Shared;
+using Sharp.Shared.CStrike;
+using Sharp.Shared.Enums;
+using Sharp.Shared.GameEntities;
+using Sharp.Shared.Types;
+
+namespace Sharp.Core.GameEntities;
+
+internal partial class PlayerCamera : BaseEntity, IPlayerCamera
+{
+    public IBasePlayerPawn? Pawn => BasePlayerPawn.Create(PawnHandle.GetEntityPtr());
+
+    public IBaseEntity? FollowEntity => BaseEntity.Create(FollowEntityHandle.GetEntityPtr());
+
+#region Schemas
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_hPawn", typeof(CEntityHandle<IBasePlayerPawn>))]
+    private partial SchemaField GetPawnHandleField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_nCameraMode", typeof(CustomCameraMode))]
+    private partial SchemaField GetCameraModeField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_hFollowEntity", typeof(CEntityHandle<IBaseEntity>))]
+    private partial SchemaField GetFollowEntityHandleField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_bFollowEyes", typeof(bool))]
+    private partial SchemaField GetFollowEyesField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_vecFollowOffset", typeof(Vector))]
+    private partial SchemaField GetFollowOffsetField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_vecCameraOffset", typeof(Vector))]
+    private partial SchemaField GetCameraOffsetField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_bClipCameraOffset", typeof(bool))]
+    private partial SchemaField GetClipCameraOffsetField();
+
+    [NativeSchemaField("CCSCustomPlayerCamera", "m_flCameraOffsetReturnStrength", typeof(float))]
+    private partial SchemaField GetCameraOffsetReturnStrengthField();
+
+#endregion
+}
---
+++ Sharp.Shared/GameEntities/IPlayerCamera.cs
@@ -0,0 +1,109 @@
+/*
+ * ModSharp
+ * Copyright (C) 2023-2026 Kxnrl. All Rights Reserved.
+ *
+ * This file is part of ModSharp.
+ * ModSharp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ModSharp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ModSharp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using Sharp.Shared.Attributes;
+using Sharp.Shared.CStrike;
+using Sharp.Shared.Enums;
+using Sharp.Shared.Types;
+
+namespace Sharp.Shared.GameEntities;
+
+/// <summary>
+///     cs_player_camera <br />
+///     Move this to control a player's view without moving their pawn.
+/// </summary>
+[NetClass("CCSPlayerCamera")]
+public interface IPlayerCamera : IBaseEntity
+{
+    /// <summary>
+    ///     m_hPawn
+    /// </summary>
+    CEntityHandle<IBasePlayerPawn> PawnHandle { get; set; }
+
+    /// <summary>
+    ///     m_hPawn
+    /// </summary>
+    IBasePlayerPawn? Pawn { get; }
+
+    /// <summary>
+    ///     m_nCameraMode
+    /// </summary>
+    CustomCameraMode CameraMode { get; set; }
+
+    /// <summary>
+    ///     m_hFollowEntity <br />
+    ///     Entity to follow when <see cref="CameraMode" /> is <see cref="CustomCameraMode.FollowPosition" />.
+    /// </summary>
+    CEntityHandle<IBaseEntity> FollowEntityHandle { get; set; }
+
+    /// <summary>
+    ///     m_hFollowEntity
+    /// </summary>
+    IBaseEntity? FollowEntity { get; }
+
+    /// <summary>
+    ///     m_bFollowEyes <br />
+    ///     Follow the eye position of the follow entity instead of its origin.
+    /// </summary>
+    bool FollowEyes { get; set; }
+
+    /// <summary>
+    ///     m_vecFollowOffset <br />
+    ///     Offset from the followed position.
+    /// </summary>
+    Vector FollowOffset { get; set; }
+
+    /// <summary>
+    ///     m_vecCameraOffset <br />
+    ///     Offset of the camera relative to the followed position, in view space (e.g. third-person shoulder).
+    /// </summary>
+    Vector CameraOffset { get; set; }
+
+    /// <summary>
+    ///     m_bClipCameraOffset <br />
+    ///     Trace the camera offset against the world so the camera does not clip into geometry.
+    /// </summary>
+    bool ClipCameraOffset { get; set; }
+
+    /// <summary>
+    ///     m_flCameraOffsetReturnStrength
+    /// </summary>
+    float CameraOffsetReturnStrength { get; set; }
+
+    /// <summary>
+    ///     Compatibility shim for the pre-2026-09 schema. Prefer <see cref="CameraMode" />.
+    /// </summary>
+    [Obsolete("m_bEnabled was removed from the schema; use CameraMode.")]
+    bool Enabled
+    {
+        get => CameraMode != CustomCameraMode.Disabled;
+        set => CameraMode = value ? CustomCameraMode.Controlled : CustomCameraMode.Disabled;
+    }
+
+    /// <summary>
+    ///     Compatibility shim for the pre-2026-09 schema. Prefer <see cref="CameraMode" />.
+    /// </summary>
+    [Obsolete("m_bIsControllingAngles was removed from the schema; use CameraMode.")]
+    bool IsControllingAngles
+    {
+        get => CameraMode == CustomCameraMode.Controlled;
+        set => CameraMode = value ? CustomCameraMode.Controlled : CustomCameraMode.ControlledPosition;
+    }
+}
